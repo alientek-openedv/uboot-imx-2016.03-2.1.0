756eed7cc1f3795c04bac538a4953fc0d7f71e08
1252699831@qq.com <1252699831@qq.com>
2021-03-10 15:40:50 +0800

    1.初始化仓库
    Signed-off-by: 1252699831@qq.com <1252699831@qq.com>

---
+++ board/freescale/mx6ullevk/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_board/freescale/mx6ullevk/built-in.o :=  arm-poky-linux-gnueabi-ld.bfd     -r -o board/freescale/mx6ullevk/built-in.o board/freescale/mx6ullevk/mx6ullevk.o 
---
+++ include/generated/timestamp_autogenerated.h
@@ -0,0 +1,4 @@
+#define U_BOOT_DATE "Dec 18 2020"
+#define U_BOOT_TIME "11:17:49"
+#define U_BOOT_TZ "+0800"
+#define U_BOOT_DMI_DATE "12/18/2020"
